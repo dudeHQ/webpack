90bf8ae6b03f90aebe25a0ff3097f6d189f58c9c
Victor Rodrigues <contato@victormkt.com.br>
2021-12-09 16:26:00 -0300

    mar japonês

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function rootStyle() {
 
 function titleComponent() {
     const elemH1 = document.createElement('h1')
-    elemH1.innerHTML = 'Hello World 5'
+    elemH1.innerHTML = 'Hokusai'
     elemH1.classList.add('title')
     return elemH1
 }
